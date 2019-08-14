623b99ef2c2fc8bf2d82947e6129fd715257b371
eduard.titov <editied@gmail.com>
2019-08-14 15:39:55 +0300

    Update configuration, Add logo/favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,54 @@
+require('../../load-env');
+
+// get env keys
+const environmentKeys = Object.keys(process.env)
+    .filter(
+        key => key.indexOf('FRAMEPAY_DOCS') === 0,
+    );
+
 module.exports = {
     port: 8088, // for local dev
     base: '/framepay-docs/', // defines github.io location under /Rebilly
     title: 'Rebilly FramePay',
     description: 'Pre-built components for your checkout flow',
-    markdown: {
-        lineNumbers: false,
-    },
+    plugins: [
+        // https://superbiger.github.io/vuepress-plugin-tabs/#install
+        'vuepress-plugin-element-tabs',
+    ],
+    head: [
+        ['link', {rel: 'icon', href: '/favicon.ico'}],
+        ['script', {src: process.env.FRAMEPAY_DOCS_FRAMEPAY_JS_REMOTE_URL}],
+    ],
     themeConfig: {
+        logo: '/favicon.ico',
         nav: [
             {text: 'Home', link: '/'},
             {text: 'Guide', link: '/guide/'},
+            {text: 'Configuration', link: '/configuration/'},
             {text: 'Reference', link: '/reference/'},
             {text: 'Examples', link: '/examples/'},
         ],
         sidebar: {
-            '/guide/': genSidebarConfig('Guide', [
-                ['multiple-methods', 'Multiple Payment Methods'],
-                ['lead-source', 'Lead Source Attribution'],
-            ]),
-            '/reference/': genSidebarConfig('Reference', [
-                ['rebilly', 'Rebilly Namespace'],
-                ['element', 'Element Instance'],
-            ]),
-            '/examples/': genSidebarConfig('Examples', []),
+            '/guide/': genSidebarConfig(
+                'Guide', [
+                    ['multiple-methods', 'Multiple Payment Methods'],
+                    ['lead-source', 'Lead Source Attribution'],
+                ],
+            ),
+            '/reference/': genSidebarConfig(
+                'Reference', [
+                    ['rebilly', 'Rebilly Namespace'],
+                    ['element', 'Element Instance'],
+                ],
+            ),
+            '/configuration/': genSidebarConfig(
+                'Configuration', [
+                    ['properties', 'Properties'],
+                ], {sidebarDepth: 3},
+            ),
+            '/examples/': genSidebarConfig(
+                'Examples', [],
+            ),
         },
         sidebarDepth: 1,
         lastUpdated: 'Last Updated',
@@ -37,13 +63,27 @@ module.exports = {
         // custom text for edit link. Defaults to "Edit this page"
         editLinkText: 'Edit this page on Github',
     },
+    // update webpack env variables
+    chainWebpack(config) {
+        config.plugin('injections')
+            .tap(([options]) => [
+                Object.assign(
+                    options,
+                    environmentKeys.reduce((memo, key) => ({
+                        ...memo,
+                        [key]: JSON.stringify(process.env[key]),
+                    }), {}),
+                ),
+            ]);
+    },
 };
 
-function genSidebarConfig(title, children = []) {
+function genSidebarConfig(title, children = [], params = {}) {
     return [
         {
             title,
             collapsable: false,
+            ...params,
             children: [
                 '',
             ].concat(children),
