2366797d8a1d70db5bc1d9eb8ae2d88ee045b9f2
Adam Altman <adam@rebilly.com>
2020-06-15 10:14:13 -0500

    Redirect to new Framepay docs

--- examples/example-publishablekey-1/index.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  Rebilly.initialize({
-    publishableKey: 'pk_sandbox_1234567890'
-  });
-  Rebilly.on('ready', function () {
-    var card = Rebilly.card.mount('#mounting-point');
-  });
-})();
-//# sourceMappingURL=index.js.map
